b4f4114e5c89688ddb72903559a30395b8f54e98
Brett Hurst <bretthurst@outlook.com>
2019-08-20 20:24:21 -0700

    default fill in text

--- index.js
+++ index.js
@@ -2,8 +2,25 @@ const CONFIG = {
     numberArray: [],
     numberPath: [],
     maximumSum: 0,
-    numberSums: []
+    numberSums: [],
 }
+
+
+document.getElementById("numbers").value = `75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23`
 
 function writeToTriangle() {
     const numbers = document.getElementById("numbers").value
@@ -45,7 +62,7 @@ function findMaxPathSum(triangle) {
             continue;
         }
 
-        if (triangle[i][previousIndex] >= triangle[i][previousIndex + 1]) {
+        if (Number(triangle[i][previousIndex]) >= Number(triangle[i][previousIndex + 1])) {
             let number = triangle[i][previousIndex]
             path.push(number)
             triangle[i][previousIndex] = `<span style="color: red;">${number}</span>`
